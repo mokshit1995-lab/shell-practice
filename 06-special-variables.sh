8533f0f8f863da9e31d51a7767d8840511dd81f2
Gunti <mokshit.gunti@dxc.com>
2025-09-25 09:33:50 +0530

    commited

--- 06-special-variables.sh
+++ 06-special-variables.sh
@@ -6,3 +6,6 @@ echo "Script name : $0"
 echo "Current Directory : $PWD"
 echo "Who is running this : $USER"
 echo "Home Directory of user : $HOME"
+echo "PID of Scrpit : $$"
+sleep 50 &
+echo "PID of the last command in background is : $!"
