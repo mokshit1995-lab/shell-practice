b90db30c1be1edd78e5b7083dec5db024a909a27
Gunti <mokshit.gunti@dxc.com>
2025-09-25 09:23:20 +0530

    commited

--- 06-special-variables.sh
+++ 06-special-variables.sh
@@ -3,3 +3,6 @@
 echo "All variables passed to the script : $@"
 echo "All variables passed to the script : $*"
 echo "Script name : $0"
+echo "Current Directory : $PWD"
+echo "Who is running this : $USER"
+echo "Home Directory of user : $HOME"
