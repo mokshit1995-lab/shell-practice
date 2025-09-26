8ff35dc6e81d466187e1ea8f2b0ffbe88cfa43d1
Gunti <mokshit.gunti@dxc.com>
2025-09-26 17:43:13 +0530

    commited

--- 07-variables2.sh
+++ 07-variables2.sh
@@ -7,7 +7,7 @@ if [ $NUMBER -lt 10 ]; then
     echo "Given number $NUMBER is less than 10"
 elif [ $NUMBER -eq 10 ]: then
     echo "Given number $NUMBER is equal to 10"
-elif
+else    
     echo "Given number $NUMBER is greater than 10"
 fi
 
