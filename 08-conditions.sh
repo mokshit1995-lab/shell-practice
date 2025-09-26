2c7e48f34958c925cf236c7281c27f79d2bd43e3
Gunti <mokshit.gunti@dxc.com>
2025-09-26 18:07:04 +0530

    commited

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -8,3 +8,17 @@ if [ $(($NUMBER % 2)) -eq 0 ]; then
 else 
     echo "Given Number $NUMBER is ODD"
 fi
+
+
+echo "Please enter a number to check if its prime or not:"
+read NUMBER1
+
+if [ $(($NUMBER1 % 2)) -eq 0 ]; then
+    echo "Given Number $NUMBER1 is Not Prime"
+elif [ $(($NUMBER1 % 3)) -eq 0 ]; then
+    echo "Given Number $NUMBER1 is Not Prime"
+elif [ $(($NUMBER1 % 5)) -eq 0 ]; then
+    echo "Given Number $NUMBER1 is Not Prime"
+else
+    echo "Given Number $NUMBER1 is Prime"
+fi
