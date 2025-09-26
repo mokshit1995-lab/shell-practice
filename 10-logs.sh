3c780efa1c2e9d420413d6244aa6ba437042a855
Gunti <mokshit.gunti@dxc.com>
2025-09-26 22:12:05 +0530

    commited

--- 10-logs.sh
+++ 10-logs.sh
@@ -2,7 +2,7 @@
 
 
 LOGS_FOLDER="/var/log/shell-script"
-SCRPIT_NAME=$( echo $0 | cut -d "." -f1 )
+SCRPIT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
 mkdir -p $LOGS_FOLDER
