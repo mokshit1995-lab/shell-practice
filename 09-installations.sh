d3ba9bb299d85192fb4fcf1e615dea38aac0fece
Gunti <mokshit.gunti@dxc.com>
2025-09-26 21:12:38 +0530

    commited

--- 09-installations.sh
+++ 09-installations.sh
@@ -8,7 +8,7 @@ if [ $USERID -ne 0 ]; then
 fi
 
 VALIDATE(){
-    if [ ($1 -ne 0) ]; then
+    if [ $1 -ne 0 ]; then
         echo "Installation Failure of $2"
     else 
         echo "Installation success of $2"
