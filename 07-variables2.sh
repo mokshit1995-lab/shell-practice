cd36a8483f58cd052d81911da7e3fcc462afe04f
Gunti <mokshit.gunti@dxc.com>
2025-09-26 17:28:29 +0530

    commited

---
+++ 07-variables2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+NUMBER=$1
+
+if [ $NUMBER -lt 1 ]; then
+    echo "Given number $NUMBER is less than 10"
+else
+    echo "Given number $NUMBER is greater than 10"
+fi
+
+
+# -gt - Greater than
+# -eq - equal to
+# -ne - not equal to
