a6cbde0aa930a9bd285bb56818c8965fedb5ba30
Gunti <mokshit.gunti@dxc.com>
2025-09-27 16:50:04 +0530

    commited

---
+++ 12-loops.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run the script with root privilage"
+    exit 1
+fi
+
+LOGS_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
+
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo "Installation Failure of $2"
+    else 
+        echo "Installation success of $2"
+    fi
+}
+
+#$@
+
+for package in $@
+do 
+    echo " Package is : $@"
+done
